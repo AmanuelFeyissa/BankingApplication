4b4df6464fcd6ccf9ed9bf4d792af9d097b98d22
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-03 23:35:35 +0300

    just TODO and some display function codes

---
+++ src/Withdraw.java
@@ -0,0 +1,9 @@
+public class Withdraw {
+    /*
+
+        TODO Display how much many they want to withdraw starting from 100 - 3000
+        TODO let the the withdraw happen if the amount they asked is equal or less than what they have in their account
+        TODO try rejecting withdrawal if the current amount is less than 25
+
+     */
+}
