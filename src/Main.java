eab7caf064b57ee717edf76184dc3ca534987af9
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-05 22:09:05 +0300

    Created Main class and added a few constructors for future inheritance

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    double balance = 0;
+    Main(double balance){
+        this.balance = balance;
+    }
+    Main(){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
