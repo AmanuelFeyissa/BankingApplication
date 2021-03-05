9580cce0a5b36fab1cd5df2d5730d220032b93bd
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-05 21:55:33 +0300

    created displayWithdraw method and askWithdraw method and put simple prompt and if/else statement

--- src/Withdraw.java
+++ src/Withdraw.java
@@ -4,7 +4,7 @@ public class Withdraw {
     /*
 
         TODO Display how much many they want to withdraw starting from 100 - 3000 100 200 500 700 1000 1200 1500 1800 2000 2300 2700 3000 
-        TODO or specify  
+        TODO or specify     DONE
         TODO let the the withdraw happen if the amount they asked is equal or less than what they have in their account
         TODO try rejecting withdrawal if the current amount is less than 25
 
