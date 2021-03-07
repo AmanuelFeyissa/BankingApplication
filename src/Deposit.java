7019a85b3a53d6b5d74c3c9f11642ff8aa3337c1
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-07 13:42:39 +0300

    Refactoring

--- src/Deposit.java
+++
@@ -1,8 +0,0 @@
-public class Deposit {
-    /*
-        TODO Ask for the account name and account number of the person they wish to deposit to
-        TODO Ask for the amount in number and in letter
-        TODO Ask for the depositor name and phone number
-
-     */
-}
