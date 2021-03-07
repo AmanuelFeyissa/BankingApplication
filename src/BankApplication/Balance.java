255eac68a806d1b235128e15ae372fc46e56105a
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-07 20:59:49 +0300

    finished editing the outputs

--- src/BankApplication/Balance.java
+++ src/BankApplication/Balance.java
@@ -4,13 +4,12 @@ public class Balance {
     double balance = 1000;
     final int ACCOUNTNUMBER = 100006060;
 
-
     void currentBalance(){
         if (balance == 0){
-            System.out.println("Your Account is Empty");
-            System.out.println("You Have A Deposit Option If You Wish To Deposit An Amount ");
+            System.out.println("\t\t\t\t\tYour Account is Empty");
+            System.out.println("\t\t\t\t\tYou Have A Deposit Option If You Wish To Deposit An Amount ");
         }else{
-            System.out.println("Your Balance Number Is " + balance);
+            System.out.println("\t\t\t\t\tYour Balance Number Is " + balance);
         }
     }
 }
