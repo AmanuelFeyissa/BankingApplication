17cc05ad315b0ed21a5363c0b420aea93574e0ad
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2022-01-02 22:26:44 +0300

    Plan for next year

--- src/BankApplication/Balance.java
+++ src/BankApplication/Balance.java
@@ -3,7 +3,7 @@ package BankApplication;
 public class Balance {
     double balance = 1000;
     final int ACCOUNTNUMBER = 100006060;
-
+// Will finish everything on 2023
     void currentBalance(){
         if (balance == 0){
             System.out.println("\t\t\t\t\tYour Account is Empty");
